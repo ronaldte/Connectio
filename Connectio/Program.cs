8584e650af255333fe769853eaf3471084eb3fcf
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-04-24 16:37:20 +0200

    Init MVC application settings

--- Connectio/Program.cs
+++ Connectio/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
