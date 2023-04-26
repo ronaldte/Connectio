96c3c9bfec93869d4b12691f818a9054dd817152
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-04-26 21:22:54 +0200

    Add create and view posts

---
+++ Connectio/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using Connectio.Data;
+using Connectio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connectio.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var post = _postRepository.GetPostById(id);
+            return View(post);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Post post)
+        {
+            if(ModelState.IsValid)
+            {
+                _postRepository.CreatePost(post);
+                return RedirectToAction("Index", new { id = post.Id });
+            }
+            return View(post);
+            
+        }
+    }
+}
