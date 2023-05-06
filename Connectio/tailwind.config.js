13eb81c051558494d1d4489287b3f35e93e63582
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-06 11:14:33 +0200

    Add safe list for css added in C# method

--- Connectio/tailwind.config.js
+++ Connectio/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.{razor,cshtml,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    safelist: [
+        'font-bold',
+        'text-orange-400',
+        'border-orange-400',
+        'border-transparent',
+        'hover:text-gray-600',
+        'hover:border-gray-300'
+    ]
 }
 
