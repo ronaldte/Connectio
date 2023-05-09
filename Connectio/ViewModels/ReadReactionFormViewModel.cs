57102fa21f65bc22dfba6fe9501c04637ab3ca26
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-09 21:46:08 +0200

    Factor reactions into partial view and add viewmodel

---
+++ Connectio/ViewModels/ReadReactionFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Connectio.ViewModels
+{
+    public class ReadReactionFormViewModel
+    {
+        public string ReactionAction { get; set; }
+        public string ReactionIcon { get; set; }
+        public int ReactionCount { get; set; }
+        public int PostId { get; set; }
+        public bool DisplayCount { get; set; }
+
+        public ReadReactionFormViewModel(string reactionAction, string reactionIcon, int reactionCount, int postId, bool displayCount)
+        {
+            ReactionAction = reactionAction;
+            ReactionIcon = reactionIcon;
+            ReactionCount = reactionCount;
+            PostId = postId;
+            DisplayCount = displayCount;
+        }
+    }
+}
