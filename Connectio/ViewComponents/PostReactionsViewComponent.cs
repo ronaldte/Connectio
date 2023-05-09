1e9eece1c157c6f9c90a3d25bc47a1afd06749a4
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-09 17:28:05 +0200

    Add ViewComponent for bookmarks on a post

---
+++ Connectio/ViewComponents/PostReactionsViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Connectio.Data;
+using Connectio.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connectio.ViewComponents
+{
+    public class PostReactionsViewComponent : ViewComponent
+    {
+        private readonly IReactionRepository _reactionRepository;
+        private readonly IPostRepository _postRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostReactionsViewComponent(IReactionRepository reactionRepository, IPostRepository postRepository, UserManager<ApplicationUser> userManager)
+        {
+            _reactionRepository = reactionRepository;
+            _postRepository = postRepository;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync(int postId)
+        {
+            var post = _postRepository.GetPostById(postId)!;
+            var user = await _userManager.GetUserAsync(UserClaimsPrincipal);
+
+            if (user == null)
+            {
+                return View(new PostReactions(postId));
+            }
+
+            var reactions = _reactionRepository.GetReactions(user, post);
+
+            return View(reactions);
+        }
+    }
+}
