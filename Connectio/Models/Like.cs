a76e095be6f0ba1c0fceee10d2dae7d565e8b941
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-09 17:56:36 +0200

    Add like model

---
+++ Connectio/Models/Like.cs
@@ -0,0 +1,11 @@
+﻿namespace Connectio.Models
+{
+    public class Like
+    {
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
