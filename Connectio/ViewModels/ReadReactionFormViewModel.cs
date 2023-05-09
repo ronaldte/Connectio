d46700e4ce6136284bc0bfb15780d7d19b5af009
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-09 22:11:46 +0200

    Dispaly number of given reactions

--- Connectio/ViewModels/ReadReactionFormViewModel.cs
+++ Connectio/ViewModels/ReadReactionFormViewModel.cs
@@ -5,14 +5,16 @@
         public string ReactionAction { get; set; }
         public string ReactionIcon { get; set; }
         public int ReactionCount { get; set; }
+        public string ReactionType { get; set; }
         public int PostId { get; set; }
         public bool DisplayCount { get; set; }
 
-        public ReadReactionFormViewModel(string reactionAction, string reactionIcon, int reactionCount, int postId, bool displayCount)
+        public ReadReactionFormViewModel(string reactionAction, string reactionIcon, int reactionCount, string reactionType, int postId, bool displayCount)
         {
             ReactionAction = reactionAction;
             ReactionIcon = reactionIcon;
             ReactionCount = reactionCount;
+            ReactionType = reactionType + (reactionCount > 1 ? "s" : string.Empty);
             PostId = postId;
             DisplayCount = displayCount;
         }
