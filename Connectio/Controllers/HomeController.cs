2804ff7760b4d9b6cce7092a12be4d006403bfc0
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-10 23:55:09 +0200

    Display followee activity on home page

--- Connectio/Controllers/HomeController.cs
+++ Connectio/Controllers/HomeController.cs
@@ -1,29 +1,63 @@
 ﻿using Connectio.Data;
+using Connectio.Models;
 using Connectio.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Connectio.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IPostRepository _postRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IReactionRepository _reactionRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(IPostRepository postRepository)
+        public HomeController(IPostRepository postRepository, UserManager<ApplicationUser> userManager, IUserRepository userRepository, IReactionRepository reactionRepository)
         {
             _postRepository = postRepository;
+            _userManager = userManager;
+            _userRepository = userRepository;
+            _reactionRepository = reactionRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var posts = _postRepository.GetAllPosts().OrderByDescending(p => p.Created);
-            var postsViewModel = new List<ReadPostViewModel>();
-
-            foreach (var post in posts)
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
             {
-                postsViewModel.Add(new ReadPostViewModel(post));
+                return NotFound();
             }
 
-            return View(postsViewModel);
+            var following = _userRepository.GetUserByUserName(user.UserName).Following;
+
+            var posts = new List<ReadPostViewModel>();
+            foreach(var followee in following)
+            {
+                var followeePosts = _postRepository.GetAllPostsByUser(followee.UserName);
+                foreach (var post in followeePosts)
+                {
+                    posts.Add(new ReadPostViewModel(post) {ActivityCreated=post.Created});
+                }
+
+                var followeeLikes = _reactionRepository.GetAllUserLikes(followee);
+                var header = $"{followee.DisplayName} (@{followee.UserName}) liked";
+                foreach(var like in followeeLikes)
+                {
+                    posts.Add(new ReadPostViewModel(like.Post) { Header = header, ActivityCreated=like.Created, ActivityType = ActivityType.Like });
+                }
+
+                var followeeComments = _reactionRepository.GetAllUserComments(followee);
+                header = $"{followee.DisplayName}(@{followee.UserName}) commented on";
+                foreach (var comment in followeeComments)
+                {
+                    posts.Add(new ReadPostViewModel(comment.Post) { Header = header, ActivityCreated=comment.Created, Comment=comment, ActivityType=ActivityType.Comment});
+                }
+            }
+
+            return View(posts.OrderByDescending(p => p.ActivityCreated).ToList());
         }
     }
 }
