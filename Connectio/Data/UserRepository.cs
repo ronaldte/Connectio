5e2c5b23cc06f2c41f71b1dcd1447485578c54ef
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-10 23:52:50 +0200

    Fix formatting issue

--- Connectio/Data/UserRepository.cs
+++ Connectio/Data/UserRepository.cs
@@ -20,8 +20,8 @@ namespace Connectio.Data
 
         public ApplicationUser? GetUserByUserName(string username)
         {
-            return _dbContext.Users.
-                Where(u => u.UserName == username)
+            return _dbContext.Users
+                .Where(u => u.UserName == username)
                 .Include(u => u.Followers)
                 .Include(u => u.Following)
                 .FirstOrDefault();
