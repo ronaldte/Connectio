0608683890d4d93c0f66a19fec57f9af53642d23
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-11 18:22:08 +0200

    Add auto scanning for mentions on new posts

--- Connectio/Controllers/PostController.cs
+++ Connectio/Controllers/PostController.cs
@@ -10,12 +10,14 @@ namespace Connectio.Controllers
     public class PostController : Controller
     {
         private readonly IPostRepository _postRepository;
+        private readonly IMentionRepository _mentionRepository;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public PostController(IPostRepository postRepository, UserManager<ApplicationUser> userManager)
+        public PostController(IPostRepository postRepository, UserManager<ApplicationUser> userManager, IMentionRepository mentionRepository)
         {
             _postRepository = postRepository;
             _userManager = userManager;
+            _mentionRepository = mentionRepository;
         }
 
         public IActionResult Index(int id)
@@ -52,8 +54,12 @@ namespace Connectio.Controllers
                 User = await _userManager.GetUserAsync(User) ?? throw new ArgumentNullException(nameof(User)),
                 Text = post.Text
             };
-                
+            
             _postRepository.CreatePost(newPost);
+
+            _mentionRepository.AddTags(newPost);
+            _mentionRepository.AddUsers(newPost);
+
             return RedirectToAction("Index", new { id = newPost.Id });
         }
     }
