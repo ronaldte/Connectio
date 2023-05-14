2feba4a088d6062f5039c11de9d67292efbc7442
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-15 01:55:49 +0200

    Restyling of reaction panelt

---
+++ Connectio/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿const path = '/Post/Index/';
+const actuall_pathname = window.location.pathname;
+
+if (!actuall_pathname.startsWith(path)) {
+    const posts = document.querySelectorAll('.connectio-post');
+
+    posts.forEach(post => {
+        post.addEventListener('click', function (event) {
+            window.location.assign(path + post.id)
+        })
+        post.classList.add("hover:cursor-pointer", "hover:bg-slate-100","transition","ease","duration-500");
+    })
+}
+
