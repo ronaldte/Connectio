a15af436ac593aa504f4de42be33ef1f45fb65a5
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-15 10:41:46 +0200

    Update tailwind colors for post card hover

--- Connectio/tailwind.config.js
+++ Connectio/tailwind.config.js
@@ -12,7 +12,14 @@ module.exports = {
         'border-orange-400',
         'border-transparent',
         'hover:text-gray-600',
-        'hover:border-gray-300'
+        'hover:border-gray-300',
+
+        //Post Card Highlight
+        'hover:cursor-pointer',
+        'hover:bg-slate-100',
+        'transition',
+        'ease',
+        'duration-500'
     ]
 }
 
