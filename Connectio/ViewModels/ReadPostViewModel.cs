5a46b0add86b25a6347a759716efafab879686d7
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-18 11:51:25 +0200

    Remove age calc plus add user view model

--- Connectio/ViewModels/ReadPostViewModel.cs
+++ Connectio/ViewModels/ReadPostViewModel.cs
@@ -10,7 +10,7 @@ namespace Connectio.ViewModels
         public string Text { get; set; } = string.Empty;
         public string PostCreated { get; set; }
         public DateTime ActivityCreated { get; set; }
-        public ApplicationUser User { get; set; }
+        public ReadUserViewModel User { get; set; }
         public string? Header { get; set; } = null;
         public Comment? Comment { get; set; } = null;
         public ActivityType ActivityType { get; set; }
@@ -22,7 +22,7 @@ namespace Connectio.ViewModels
             Id = post.Id;
             Text = HightlightMentions(post.Text);
             PostCreated = post.Created.TimeSinceCreated();
-            User = post.User;
+            User = new ReadUserViewModel(post.User);
         }
 
         private static string HightlightMentions(string text)
@@ -30,36 +30,6 @@ namespace Connectio.ViewModels
             string pattern = @"\B([#@][a-zA-Z0-9(_)]+\b)";
             string replace = """<strong class="text-orange-400">$&</strong>""";
             return Regex.Replace(text, pattern, replace);
-        }
-
-        private static string CalculateAge(DateTime created)
-        {
-            var timeDifference = DateTime.UtcNow - created;
-
-            string response;
-
-            if (timeDifference.TotalDays > 365)
-            {
-                response = created.ToString("MMM yyyy");
-            }
-            else if (timeDifference.TotalHours > 24)
-            {
-                response = created.ToString("d MMM");
-            }
-            else if(timeDifference.TotalHours > 1)
-            {
-                response = $"{timeDifference.Hours}h";
-            }
-            else if( timeDifference.TotalMinutes > 1)
-            {
-                response = $"{timeDifference.Minutes}m";
-            }
-            else
-            {
-                response = "Just now";
-            }
-
-            return response;
         }
     }
 }
