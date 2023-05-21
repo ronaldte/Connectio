c9497f9362fd57fb15d239c866557e08a637b4f0
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-21 21:49:43 +0200

    Add view component for views

---
+++ Connectio/ViewComponents/TrendViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Connectio.Data;
+using Connectio.Models;
+using Connectio.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connectio.ViewComponents
+{
+    public class TrendViewComponent : ViewComponent
+    {
+        private readonly ITrendRepository _trendRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TrendViewComponent(ITrendRepository trendRepository, UserManager<ApplicationUser> userManager)
+        {
+            _trendRepository = trendRepository;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(UserClaimsPrincipal) ?? throw new UnauthorizedAccessException();
+
+            var peopleToFollow = _trendRepository.GetPeopleToFollow(user).Select(u => new ReadUserViewModel(u));
+            var trendingTags = _trendRepository.GetTrendingTags().Select(t => new ReadTagViewModel(t));
+
+            var viewModel = new ReadTrendViewModel(peopleToFollow, trendingTags);
+            return View(viewModel);
+        }
+    }
+}
---
+++ Connectio/ViewModels/ReadTrendViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Connectio.ViewModels
+{
+    public class ReadTrendViewModel
+    {
+        public IEnumerable<ReadUserViewModel> PeopleToFollow { get; set; }
+        public IEnumerable<ReadTagViewModel> TrendingTags { get; set; }
+
+        public ReadTrendViewModel(IEnumerable<ReadUserViewModel> peopleToFollow, IEnumerable<ReadTagViewModel> trendingTags)
+        {
+            PeopleToFollow = peopleToFollow;
+            TrendingTags = trendingTags;
+        }
+    }
+}
