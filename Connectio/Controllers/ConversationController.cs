895a38e0574a4c5b31cab8c851617e593a75b591
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-23 23:56:55 +0200

    Add Conversation controller

---
+++ Connectio/Controllers/ConversationController.cs
@@ -0,0 +1,24 @@
+﻿using Connectio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connectio.Controllers
+{
+    public class ConversationController : Controller
+    {       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string username)
+        {
+            return View();
+        }
+        
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
