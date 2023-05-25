a7424c4cf1a03529e1c430fa08d1538c90e287f4
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-25 23:36:26 +0200

    Add conversation view model for creating new ones

---
+++ Connectio/ViewModels/CreateConversationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Connectio.ViewModels
+{
+    public class CreateConversationViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        public string ToUser { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Message")]
+        [StringLength(512, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        public string MessageText { get; set; } = string.Empty;
+    }
+}
