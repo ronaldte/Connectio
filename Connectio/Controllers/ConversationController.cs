0f9f82442217747defbe66f29a0d7809dd51363e
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-25 23:38:25 +0200

    Add validation to new conversation

--- Connectio/Controllers/ConversationController.cs
+++ Connectio/Controllers/ConversationController.cs
@@ -1,18 +1,42 @@
-﻿using Connectio.Models;
+﻿using Connectio.Data;
+using Connectio.Models;
+using Connectio.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Connectio.Controllers
 {
+    [Authorize]
     public class ConversationController : Controller
-    {       
+    {
+        private readonly IUserRepository _userRepository;
+
+        public ConversationController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(string username)
+        public IActionResult Create(CreateConversationViewModel newConversation)
         {
+            if(!ModelState.IsValid)
+            {
+                return View(newConversation);
+            }
+
+            var userToMessage = _userRepository.GetUserByUserName(newConversation.ToUser);
+            if(userToMessage is null)
+            {
+                ModelState.AddModelError(nameof(newConversation.ToUser), "This user does not exist");
+                return View(newConversation);
+            }
+
             return View();
         }
         
