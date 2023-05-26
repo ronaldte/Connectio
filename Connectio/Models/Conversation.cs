49e9c2677f8e1054068fc8122576197a6e4eb5e8
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-26 12:40:19 +0200

    Add messages model

--- Connectio/Models/Conversation.cs
+++ Connectio/Models/Conversation.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public bool IsPrivate { get; set; } = true;
         public List<ApplicationUser> Participants { get; set; } = new();
+        public List<Message> Messages { get; set; } = new();
     }
 }
---
+++ Connectio/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Connectio.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public ApplicationUser CreatedBy { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; } = null!;
+        public Conversation Conversation { get; set; } = null!;
+    }
+}
