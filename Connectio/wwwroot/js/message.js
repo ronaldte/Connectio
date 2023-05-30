8d9310bee0e6a8cc9937faa0e16cd4e7b1a1ce70
Ronald Telmanik <60744121+ronaldte@users.noreply.github.com>
2023-05-30 10:50:57 +0200

    Display message bubble for some time on new message

--- Connectio/wwwroot/js/message.js
+++ Connectio/wwwroot/js/message.js
@@ -1,9 +1,33 @@
 ﻿"use strict";
 
+// Returns timer for 10 seconds to autohide notification bubble.
+function get_notification_timer() {
+    return setTimeout(() => {
+        document.getElementById("notification-bubble").hidden = true
+    }, 10000);
+}
+
+// Global reference to timer on notification bubble.
+var notification_timer = null;
+
 var connection = new signalR.HubConnectionBuilder().withUrl("/messageHub").build();
 
+// Updates notification bubble on new message and runs countdown to autohide.
 connection.on("Notify", function (user, message) {
-    alert(user + " says " + message);
+    clearTimeout(notification_timer);
+
+    var bubble = document.getElementById("notification-bubble");
+    bubble.hidden = false;
+
+    var user_element = document.getElementById("notification-from");
+    user_element.innerHTML = user;
+
+    var message_element = document.getElementById("notification-message");
+    const short_message = ((message.length < 51) ? message : message.substring(0, 50) + "...");
+    message_element.innerHTML = short_message;
+
+    notification_timer = get_notification_timer();
+
 });
 
 connection.start();
